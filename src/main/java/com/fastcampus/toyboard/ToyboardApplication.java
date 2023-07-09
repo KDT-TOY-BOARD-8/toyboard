2fff805f237cd8a2e411fbeae4c0c97ed2181184
YongHo Shin <gdtknight@naver.com>
2023-07-10 02:30:53 +0900

    update : format ToyboardApplication class

--- src/main/java/com/fastcampus/toyboard/ToyboardApplication.java
+++ src/main/java/com/fastcampus/toyboard/ToyboardApplication.java
@@ -8,8 +8,7 @@ import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 @ConfigurationPropertiesScan
 public class ToyboardApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ToyboardApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(ToyboardApplication.class, args);
+  }
 }
