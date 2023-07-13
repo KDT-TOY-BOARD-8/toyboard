bbb907074774bac0134c5b5294dbd131a212cc3c
ho30archive <ho30archive@gmail.com>
2023-07-13 16:45:50 +0900

    feat : user domain setting

---
+++ src/main/java/com/fastcampus/toyboard/user/domain/User.java
@@ -0,0 +1,48 @@
+package com.fastcampus.toyboard.user.domain;
+
+import static javax.persistence.EnumType.STRING;
+
+
+import com.fastcampus.toyboard.common.BaseTimeEntity;
+import com.fastcampus.toyboard.enums.RoleType;
+import java.sql.Time;
+import java.sql.Timestamp;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder // Builder는 NoArgsConstructor를 쓸 때 AllArgsConstructor가 없으면 안된다.
+public class User extends BaseTimeEntity {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private int id;
+
+  @Column(nullable = false, length = 20, unique = true) // 네이버기준 20글자
+  private String username;
+
+  @Column(nullable = false, length = 20) // 네이버기준 20글자
+  private String password;
+
+  @Column(nullable = false, length = 35) // 네이버기준 아이디 20글자 + 주소15자리
+  private String email;
+
+  @Column(nullable = false, length = 10)
+  private String nickName;
+
+  @Enumerated(EnumType.STRING)
+  @Column(nullable = false, length = 10)
+  private RoleType role;
+}
