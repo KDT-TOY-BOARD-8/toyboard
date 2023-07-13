0ed98905aad1188198509355e6464b824883d4fb
YongHo Shin <gdtknight@naver.com>
2023-07-13 14:42:20 +0900

    Thymeleaf Template Engine 추가 및 기본 설정 완료 (#4)
    * feat : add ThymeleafConfig class - thymeleaf decoupled logic
    
    * update : build.gradle - formatting 새로 적용
    
    * update : build.gradle - thymeleaf dependency 적용
    
    * update : application.yml - thymeleaf decoupled logic property 설정
    
    * update : add @ConfigurationPropertiesScan
    
    * update : format ToyboardApplication class
    
    * update : application.yml - devtools Live Reload 기능 활성화

--- src/main/java/com/fastcampus/toyboard/ToyboardApplication.java
+++ src/main/java/com/fastcampus/toyboard/ToyboardApplication.java
@@ -2,12 +2,13 @@ package com.fastcampus.toyboard;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class ToyboardApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ToyboardApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(ToyboardApplication.class, args);
+  }
 }
