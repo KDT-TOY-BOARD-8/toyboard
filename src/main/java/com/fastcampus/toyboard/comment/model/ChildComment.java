4b92aa267bcf81ea9b092f82fdaac34316cee6ad
YongHo Shin <gdtknight@naver.com>
2023-07-13 16:04:56 +0900

    feat : add ChildComment entity class

---
+++ src/main/java/com/fastcampus/toyboard/comment/model/ChildComment.java
@@ -0,0 +1,27 @@
+package com.fastcampus.toyboard.comment.model;
+
+import java.time.LocalDateTime;
+import javax.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Entity(name = "child_comment")
+@EntityListeners(AuditingEntityListener.class)
+@NoArgsConstructor
+@Getter
+public class ChildComment {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  Long id;
+
+  @Column String content;
+
+  @ManyToOne Comment comment;
+
+  @CreatedDate LocalDateTime createdAt;
+
+  @LastModifiedDate LocalDateTime updatedAt;
+}
