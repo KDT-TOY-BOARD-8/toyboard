55418b3ed8b617fe2f7223d6110da6e792147071
yangsoohyun <cendy1234@naver.com>
2023-07-14 16:22:07 +0900

    add : 게시글 목록 보기 조회 기능 추가

--- src/main/java/com/fastcampus/toyboard/board/repository/BoardRepository.java
+++ src/main/java/com/fastcampus/toyboard/board/repository/BoardRepository.java
@@ -2,10 +2,12 @@ package com.fastcampus.toyboard.board.repository;
 
 import com.fastcampus.toyboard.board.model.Board;
 import com.fastcampus.toyboard.board.model.BoardType;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface BoardRepository extends JpaRepository<Board, Long> {
     List<Board> findByBoardType(BoardType boardType);
+    List<Board> findAll(Sort sort);
 }
