34701ef3af97ca92e9494b2af4ad5bd0f408b809
YongHo Shin <gdtknight@naver.com>
2023-07-15 19:03:45 +0900

    update : SecurityConfig - Allow static resource access

--- src/main/java/com/fastcampus/toyboard/config/SecurityConfig.java
+++ src/main/java/com/fastcampus/toyboard/config/SecurityConfig.java
@@ -3,10 +3,17 @@ package com.fastcampus.toyboard.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
+
+  @Bean
+  public WebSecurityCustomizer webSecurityCustomizer() {
+    return (web) -> web.ignoring().antMatchers("/assets/**");
+  }
+
   @Bean
   public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
     http.csrf().disable();
