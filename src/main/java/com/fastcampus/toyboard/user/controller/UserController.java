1f611761f6b3e91b36bb5758c83e32ca9ab2edaa
YongHo Shin <gdtknight@naver.com>
2023-07-15 19:54:33 +0900

    update : UserController - add Login, SignUp PostMapping

--- src/main/java/com/fastcampus/toyboard/user/controller/UserController.java
+++ src/main/java/com/fastcampus/toyboard/user/controller/UserController.java
@@ -1,17 +1,34 @@
 package com.fastcampus.toyboard.user.controller;
 
+import com.fastcampus.toyboard.user.dto.UserRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class UserController {
-    @GetMapping("/login")
-    public String login() {
-        return "login";
-    }
+  @GetMapping("/login")
+  public String login() {
+    return "login";
+  }
 
-    @GetMapping("/sign-up")
-    public String signUp() {
-        return "sign-up";
-    }
+  @ResponseBody
+  @PostMapping("/login")
+  public UserRequest.LoginDto login(UserRequest.LoginDto loginDto) {
+    System.out.println(loginDto);
+    return loginDto;
+  }
+
+  @GetMapping("/sign-up")
+  public String signUp() {
+    return "sign-up";
+  }
+
+  @ResponseBody
+  @PostMapping("/sign-up")
+  public UserRequest.SignUpDto login(UserRequest.SignUpDto signUpDto) {
+    System.out.println(signUpDto);
+    return signUpDto;
+  }
 }
