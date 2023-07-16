cd15db4cd1a1467da53e0054c8c77dc8aa9d5f42
yangsoohyun <cendy1234@naver.com>
2023-07-16 19:41:41 +0900

    add : 게시글 검색 쿼리 메서드 추가

--- src/main/java/com/fastcampus/toyboard/board/repository/BoardRepository.java
+++ src/main/java/com/fastcampus/toyboard/board/repository/BoardRepository.java
@@ -4,12 +4,12 @@ import com.fastcampus.toyboard.board.model.Board;
 import com.fastcampus.toyboard.board.model.BoardType;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface BoardRepository extends JpaRepository<Board, Long> {
     List<Board> findByBoardType(BoardType boardType);
-    Page<Board> findAll(Pageable pageable);
+    Page<Board> findByTitleContainingOrContentContainingOrNickNameContaining(String title, String content, String nickName, Pageable pageable);
+
 }
