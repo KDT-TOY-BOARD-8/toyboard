cab7345421a7fd1f7a975a79be9f17f1fc08c364
YongHo Shin <gdtknight@naver.com>
2023-07-17 17:18:28 +0900

    update : SecurityConfig

--- src/main/java/com/fastcampus/toyboard/config/security/SecurityConfig.java
+++ src/main/java/com/fastcampus/toyboard/config/security/SecurityConfig.java
@@ -1,7 +1,6 @@
 package com.fastcampus.toyboard.config.security;
 
 import com.fastcampus.toyboard.user.repository.BoardUserRepository;
-import com.fastcampus.toyboard.user.service.BoardUserManager;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
