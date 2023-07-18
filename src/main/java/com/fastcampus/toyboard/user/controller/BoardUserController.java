a1068a8ab22688d7d2a9d61c408e37bfa4053935
ho30archive <ho30archive@gmail.com>
2023-07-18 16:32:07 +0900

    feat : 회원가입시 중복체크 컨트롤러 추가 (추후 수정 가능성 높음)

--- src/main/java/com/fastcampus/toyboard/user/controller/BoardUserController.java
+++ src/main/java/com/fastcampus/toyboard/user/controller/BoardUserController.java
@@ -3,8 +3,10 @@ package com.fastcampus.toyboard.user.controller;
 import com.fastcampus.toyboard.user.dto.BoardUserRequest;
 import com.fastcampus.toyboard.user.service.BoardUserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
@@ -37,5 +39,11 @@ public class BoardUserController {
     System.out.println(boardUserService.signUp(signUpDto));
 
     return "redirect:/";
+  }
+
+
+  @GetMapping("/sign-up/{username}/exists")
+  public ResponseEntity<Boolean> checkUsernameDuplicate(@PathVariable String username) {
+    return ResponseEntity.ok(boardUserService.usernameOverlap2(username));
   }
 }
