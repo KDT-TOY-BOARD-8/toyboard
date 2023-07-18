af3b2a39ef2c5cce3554b8b8e2d871d2f2a73a41
ho30archive <ho30archive@gmail.com>
2023-07-19 00:11:50 +0900

    feat : ~exists 컨트롤러 추가

--- src/main/java/com/fastcampus/toyboard/user/controller/BoardUserController.java
+++ src/main/java/com/fastcampus/toyboard/user/controller/BoardUserController.java
@@ -1,9 +1,12 @@
 package com.fastcampus.toyboard.user.controller;
 
+import com.fastcampus.toyboard.user.dto.BoardUserDto;
 import com.fastcampus.toyboard.user.dto.BoardUserRequest;
+import com.fastcampus.toyboard.user.model.BoardUser;
 import com.fastcampus.toyboard.user.service.BoardUserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,47 +16,46 @@ import org.springframework.web.bind.annotation.PostMapping;
 @RequiredArgsConstructor
 public class BoardUserController {
 
-  private final BoardUserService boardUserService;
+    private final BoardUserService boardUserService;
 
-  @GetMapping("/login")
-  public String login() {
-    return "login";
-  }
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
 
-  @PostMapping("/login")
-  public String login(
-      BoardUserRequest.LoginDto loginDto) {
-    System.out.println(
-        "Username : " + loginDto.getUsername() + ", Password : " + loginDto.getPassword());
-    System.out.println("Login Success.");
+    @PostMapping("/login")
+    public String login(
+        BoardUserRequest.LoginDto loginDto) {
+        System.out.println(
+            "Username : " + loginDto.getUsername() + ", Password : " + loginDto.getPassword());
+        System.out.println("Login Success.");
 
-    return "redirect:/";
-  }
+        return "redirect:/";
+    }
 
-  @GetMapping("/sign-up")
-  public String signUp() {
-    return "sign-up";
-  }
+    @GetMapping("/sign-up")
+    public String signUp() {
+        return "sign-up";
+    }
 
-  @PostMapping("/sign-up")
-  public String register(BoardUserRequest.SignUpDto signUpDto) {
-    System.out.println(boardUserService.signUp(signUpDto));
+    @PostMapping("/sign-up")
+    public String register(BoardUserRequest.SignUpDto signUpDto) {
+        System.out.println(boardUserService.signUp(signUpDto));
 
-    return "redirect:/";
-  }
-
-
-  @GetMapping("/sign-up/{username}/exists")
-  public ResponseEntity<Boolean> checkUsernameDuplicate(@PathVariable String username) {
-    return ResponseEntity.ok(boardUserService.usernameOverlap(username));
-  }
+        return "redirect:/";
+    }
 
 
-  @GetMapping("/sign-up/{email}/exists")
-  public ResponseEntity<Boolean> checkEmailDuplicate(@PathVariable String email) {
-    return ResponseEntity.ok(boardUserService.emailOverlap(email));
-  }
+    @GetMapping("/sign-up/{username}/usernameExists")
+    public ResponseEntity<Boolean> checkUsernameDuplicate(@PathVariable String username) {
+        return ResponseEntity.ok(boardUserService.usernameOverlap(username));
+    }
 
+
+    @GetMapping("/sign-up/{email}/emailExists")
+    public ResponseEntity<Boolean> checkEmailDuplicate(@PathVariable String email) {
+        return ResponseEntity.ok(boardUserService.emailOverlap(email));
+    }
 
 
 }
