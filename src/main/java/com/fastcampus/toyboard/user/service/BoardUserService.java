19dad21e7ad12f337230ce5738b3ebf48b2da525
ho30archive <ho30archive@gmail.com>
2023-07-19 16:13:43 +0900

    feat : editMyInfo 개인정보 수정 서비스 추가

--- src/main/java/com/fastcampus/toyboard/user/service/BoardUserService.java
+++ src/main/java/com/fastcampus/toyboard/user/service/BoardUserService.java
@@ -6,9 +6,8 @@ import com.fastcampus.toyboard.user.model.BoardAuthority;
 import com.fastcampus.toyboard.user.model.BoardUser;
 import com.fastcampus.toyboard.user.repository.BoardAuthorityRepository;
 import com.fastcampus.toyboard.user.repository.BoardUserRepository;
-import com.sun.xml.bind.v2.TODO;
-import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
@@ -23,92 +22,95 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional
 @RequiredArgsConstructor
 public class BoardUserService implements UserDetailsService {
-  private final BoardUserRepository boardUserRepository;
-  private final BoardAuthorityRepository boardAuthorityRepository;
-  private final PasswordEncoder passwordEncoder;
 
-  @Override
-  public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-    return boardUserRepository
-        .findBoardUserByUsername(username)
-        .orElseThrow(
-            () -> {
-              throw new UsernameNotFoundException(username);
-            });
-  }
+    private final BoardUserRepository boardUserRepository;
+    private final BoardAuthorityRepository boardAuthorityRepository;
+    private final PasswordEncoder passwordEncoder;
 
-  public BoardUserDto signUp(BoardUserRequest.SignUpDto signUpDto) {
-    BoardUserDto newUserDto = BoardUserDto.of(signUpDto);
-
-    // 패스워드 암호화 후 유저 정보 저장
-    newUserDto.setPassword(passwordEncoder.encode(newUserDto.getPassword()));
-    BoardUser newBoardUser = boardUserRepository.save(BoardUser.of(newUserDto));
-
-    // 새로 가입한 경우 새싹 유저 권한 부여
-    BoardAuthority newRole = new BoardAuthority(newBoardUser.getUserId(), "SPROUT");
-    boardAuthorityRepository.save(newRole);
-
-    return BoardUserDto.fromEntity(
-        boardUserRepository
-            .findBoardUserByUsername(newBoardUser.getUsername())
-            .orElseThrow(
-                () -> {
-                  throw new RuntimeException("Sign Up Error.");
-                }));
-  }
-
-  private BoardUser save(BoardUser boardUser) {
-    return boardUserRepository.save(boardUser);
-  }
-
-  public BoardUserDto findUser(String username) {
-    return BoardUserDto.fromEntity(
-        boardUserRepository
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return boardUserRepository
             .findBoardUserByUsername(username)
             .orElseThrow(
                 () -> {
-                  throw new UsernameNotFoundException("Can't find user : " + username);
-                }));
-  }
+                    throw new UsernameNotFoundException(username);
+                });
+    }
 
-  public void addAuthority(Long userId, String authority) {
-    boardUserRepository
-        .findById(userId)
-        .ifPresent(
-            user -> {
-              BoardAuthority newRole = new BoardAuthority(user.getUserId(), authority);
-              Set<BoardAuthority> authorities = new HashSet<>();
-              if (user.getAuthorities() == null) {
-                authorities.add(newRole);
-              } else if (!user.getAuthorities().contains(newRole)) {
-                authorities.addAll(user.getAuthorities());
-                authorities.add(newRole);
-              }
-              user.setAuthorities(authorities);
-              save(user);
-            });
-  }
+    public BoardUserDto signUp(BoardUserRequest.SignUpDto signUpDto) {
+        BoardUserDto newUserDto = BoardUserDto.of(signUpDto);
 
-  public void removeAuthority(Long userId, String authority) {
-    boardUserRepository
-        .findById(userId)
-        .ifPresent(
-            user -> {
-              if (user.getAuthorities() == null) return;
+        // 패스워드 암호화 후 유저 정보 저장
+        newUserDto.setPassword(passwordEncoder.encode(newUserDto.getPassword()));
+        BoardUser newBoardUser = boardUserRepository.save(BoardUser.of(newUserDto));
 
-              BoardAuthority targetRole = new BoardAuthority(user.getUserId(), authority);
+        // 새로 가입한 경우 새싹 유저 권한 부여
+        BoardAuthority newRole = new BoardAuthority(newBoardUser.getUserId(), "SPROUT");
+        boardAuthorityRepository.save(newRole);
 
-              if (user.getAuthorities().contains(targetRole)) {
-                user.setAuthorities(
-                    user.getAuthorities().stream()
-                        .filter(auth -> !auth.equals(targetRole))
-                        .collect(Collectors.toSet()));
-                save(user);
-              }
-            });
-  }
+        return BoardUserDto.fromEntity(
+            boardUserRepository
+                .findBoardUserByUsername(newBoardUser.getUsername())
+                .orElseThrow(
+                    () -> {
+                        throw new RuntimeException("Sign Up Error.");
+                    }));
+    }
 
-    // TODO: 2023/07/18 프론트에 중복체크 버튼 생성해야함. 컨트롤러 연결 어떻게 할까...
+    private BoardUser save(BoardUser boardUser) {
+        return boardUserRepository.save(boardUser);
+    }
+
+    public BoardUserDto findUser(String username) {
+        return BoardUserDto.fromEntity(
+            boardUserRepository
+                .findBoardUserByUsername(username)
+                .orElseThrow(
+                    () -> {
+                        throw new UsernameNotFoundException("Can't find user : " + username);
+                    }));
+    }
+
+    public void addAuthority(Long userId, String authority) {
+        boardUserRepository
+            .findById(userId)
+            .ifPresent(
+                user -> {
+                    BoardAuthority newRole = new BoardAuthority(user.getUserId(), authority);
+                    Set<BoardAuthority> authorities = new HashSet<>();
+                    if (user.getAuthorities() == null) {
+                        authorities.add(newRole);
+                    } else if (!user.getAuthorities().contains(newRole)) {
+                        authorities.addAll(user.getAuthorities());
+                        authorities.add(newRole);
+                    }
+                    user.setAuthorities(authorities);
+                    save(user);
+                });
+    }
+
+    public void removeAuthority(Long userId, String authority) {
+        boardUserRepository
+            .findById(userId)
+            .ifPresent(
+                user -> {
+                    if (user.getAuthorities() == null) {
+                        return;
+                    }
+
+                    BoardAuthority targetRole = new BoardAuthority(user.getUserId(), authority);
+
+                    if (user.getAuthorities().contains(targetRole)) {
+                        user.setAuthorities(
+                            user.getAuthorities().stream()
+                                .filter(auth -> !auth.equals(targetRole))
+                                .collect(Collectors.toSet()));
+                        save(user);
+                    }
+                });
+    }
+
+    // TODO: 2023/07/18 프론트에 중복체크 버튼 생성해야함, 연결은 마지막에
 
     @Transactional(readOnly = true)
     public boolean usernameOverlap(String username) {
@@ -128,6 +130,33 @@ public class BoardUserService implements UserDetailsService {
         boolean nicknameDuplicate = boardUserRepository.existsByNickname(nickname);
         return nicknameDuplicate;
     }
+
+
+    @Transactional
+    public BoardUserDto editMyInfo(BoardUserRequest.EditDto editDto, BoardUser boardUser) {
+        System.out.println("testEdit : ");
+        System.out.println("nickname : " + editDto.getNickname());
+        System.out.println("email : " + editDto.getEmail());
+        if (!editDto.getEmail().isEmpty()) {
+            boardUser.updateEmail(editDto.getEmail());
+        }
+
+        if (!editDto.getNickname().isEmpty()) {
+            boardUser.updateNickname(editDto.getNickname());
+        }
+        boardUserRepository.save(boardUser);
+        return BoardUserDto.fromEntity(
+            boardUserRepository
+                .findBoardUserByUsername(boardUser.getUsername())
+                .orElseThrow(
+                    () -> {
+                        throw new RuntimeException("Update Error.");
+                    }));
+    }
+
+
+
+
 
 }
 
