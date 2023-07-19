0baab877a279e6a2e79615fa0256cc027c1c1d84
ho30archive <ho30archive@gmail.com>
2023-07-19 16:16:40 +0900

    feat : editDto 추가

--- src/main/java/com/fastcampus/toyboard/user/dto/BoardUserRequest.java
+++ src/main/java/com/fastcampus/toyboard/user/dto/BoardUserRequest.java
@@ -3,19 +3,31 @@ package com.fastcampus.toyboard.user.dto;
 import lombok.*;
 
 public class BoardUserRequest {
-  @AllArgsConstructor
-  @Getter
-  public static class LoginDto {
-    private final String username;
-    private final String password;
-  }
 
-  @AllArgsConstructor
-  @Getter
-  public static class SignUpDto {
-    private final String username;
-    private final String password;
-    private final String email;
-    private final String nickname;
-  }
+    @AllArgsConstructor
+    @Getter
+    public static class LoginDto {
+
+        private final String username;
+        private final String password;
+    }
+
+    @AllArgsConstructor
+    @Getter
+    public static class SignUpDto {
+
+        private final String username;
+        private final String password;
+        private final String email;
+        private final String nickname;
+    }
+
+    @AllArgsConstructor
+    @Getter
+    public static class EditDto {
+
+        private final String password;
+        private final String email;
+        private final String nickname;
+    }
 }
