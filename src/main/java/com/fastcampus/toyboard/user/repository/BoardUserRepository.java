b05ae0fccbc702d856d043662a2935b03dc95862
ho30archive <ho30archive@gmail.com>
2023-07-19 16:14:56 +0900

    fix : 정렬

--- src/main/java/com/fastcampus/toyboard/user/repository/BoardUserRepository.java
+++ src/main/java/com/fastcampus/toyboard/user/repository/BoardUserRepository.java
@@ -6,13 +6,15 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface BoardUserRepository extends JpaRepository<BoardUser, Long> {
-  Optional<BoardUser> findBoardUserByNickname(String nickname);
 
-  Optional<BoardUser> findBoardUserByUsername(String username);
+    Optional<BoardUser> findBoardUserByNickname(String nickname);
 
-  boolean existsByUsername(String username);
-  boolean existsByEmail(String email);
+    Optional<BoardUser> findBoardUserByUsername(String username);
 
-  boolean existsByNickname(String nickname);
+    boolean existsByUsername(String username);
+
+    boolean existsByEmail(String email);
+
+    boolean existsByNickname(String nickname);
 
 }
