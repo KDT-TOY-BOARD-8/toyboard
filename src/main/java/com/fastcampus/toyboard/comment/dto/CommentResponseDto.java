acc6feb0ef7ca1a5b8d354fc8b941994dc5af5ea
YongHo Shin <gdtknight@naver.com>
2023-07-20 19:59:13 +0900

    feat : add CommentResponseDto, ChildCommentResponseDto

---
+++ src/main/java/com/fastcampus/toyboard/comment/dto/CommentResponseDto.java
@@ -0,0 +1,36 @@
+package com.fastcampus.toyboard.comment.dto;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.ToString;
+
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@ToString
+@Getter
+public class CommentResponseDto {
+  private Long commentId;
+  private String content;
+  private Long boardId;
+  private String nickname;
+
+  private List<ChildCommentResponseDto> childCommentDtos;
+
+  public static CommentResponseDto of(Long id, String content, Long boardId, String nickName) {
+    return new CommentResponseDto(id, content, boardId, nickName, null);
+  }
+
+  public static CommentResponseDto of(CommentDto commentDto) {
+    return new CommentResponseDto(
+        commentDto.getCommentId(),
+        commentDto.getContent(),
+        commentDto.getBoardDto().getBoardId(),
+        commentDto.getBoardUserDto().getNickname(),
+        commentDto.getChildCommentDtos().stream()
+            .map(ChildCommentResponseDto::of)
+            .collect(Collectors.toList()));
+  }
+}
